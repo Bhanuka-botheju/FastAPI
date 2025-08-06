5c7fa475ef365b5dd2bcb37b2fb05ad19ef26d05
Bhanuka-botheju <wpbhanukabotheju@gmail.com>
2025-08-06 09:18:07 +0530

    Implement blog creation endpoint with database integration

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from . import schemas
 from . import models
-from . database import engine
+from . database import engine , SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post("/blog")
-async def create_blog(request: schemas.Blog):
-    return {'title': request.title, 'content': request.content}
+async def create_blog(request: schemas.Blog , db:Session = Depends(get_db)):
+    new_blog = models.Blog(title =request.title, body=request.content)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
