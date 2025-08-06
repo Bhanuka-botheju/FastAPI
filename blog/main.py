26a6be9a92f62e17b898df6aaa33f06d68cdab20
Bhanuka-botheju <wpbhanukabotheju@gmail.com>
2025-08-06 09:04:21 +0530

    Refactor database setup and add Blog model with SQLAlchemy

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from . import schemas
+from . import models
+from . database import engine
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.post("/blog")
