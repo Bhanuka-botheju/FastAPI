d9e86ba01fcfd83f338cc9ef0eee36f35d1e2359
Bhanuka-botheju <wpbhanukabotheju@gmail.com>
2025-08-09 11:26:30 +0530

    Add tags to blog and user endpoints; update models for user-blog relationship

--- blog/main.py
+++ blog/main.py
@@ -18,15 +18,15 @@ async def get_db():
     finally:
         db.close()
 
-@app.post("/blog",status_code =status.HTTP_201_CREATED)
+@app.post("/blog",status_code =status.HTTP_201_CREATED , tags=["Blogs"])
 async def create_blog(request: schemas.Blog , db:Session = Depends(get_db)):
-    new_blog = models.Blog(title =request.title, body=request.content)
+    new_blog = models.Blog(title =request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT , tags=["Blogs"])
 async def delete_blog_by_id(id , db:Session = Depends(get_db)):
     blog_by_id = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     if not blog_by_id:
@@ -35,7 +35,7 @@ async def delete_blog_by_id(id , db:Session = Depends(get_db)):
     db.commit()
     return {"detail": f"Blog with id {id} deleted successfully"}
 
-@app.put('/blog/{id}' , status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}' , status_code=status.HTTP_202_ACCEPTED , tags=["Blogs"])
 async def update_by_id(id,request:schemas.Blog , db:Session = Depends(get_db)):
     blog_by_id = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog_by_id:
@@ -46,12 +46,12 @@ async def update_by_id(id,request:schemas.Blog , db:Session = Depends(get_db)):
     db.refresh(blog_by_id)
     return blog_by_id
 
-@app.get('/blog',status_code = 200 , response_model=List[schemas.ShowBlog])
+@app.get('/blog',status_code = 200 , response_model=List[schemas.ShowBlog] , tags=["Blogs"])
 async def get_blogs_all(db:Session = Depends(get_db)):
     all_blogs = db.query(models.Blog).all()
     return all_blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK,response_model = schemas.ShowBlog )
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK,response_model = schemas.ShowBlog , tags=["Blogs"])
 async def get_blog_by_id(id: int, db: Session = Depends(get_db)):
     blog_by_id = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog_by_id:
@@ -61,7 +61,7 @@ async def get_blog_by_id(id: int, db: Session = Depends(get_db)):
         )
     return blog_by_id
 
-@app.post('/user',status_code = 200 )
+@app.post('/user',status_code = 200 , tags=["Users"]) 
 async def create_user(request:schemas.User , db:Session = Depends(get_db)):
     exist_user = db.query(models.User).filter(models.User.email == request.email).first()
     if exist_user:
@@ -73,21 +73,21 @@ async def create_user(request:schemas.User , db:Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user',status_code = 200 , response_model = List[schemas.ShowUser])
+@app.get('/user',status_code = 200 , response_model = List[schemas.ShowUser],tags=["Users"])
 async def get_all_users(db:Session = Depends(get_db)):
     all_users = db.query(models.User).all()
     if not all_users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No users found")
     return all_users
 
-@app.get('/user/{id}',status_code=200)
+@app.get('/user/{id}',status_code=200,tags=["Users"])
 async def get_user_by_id(id : int,db:Session = Depends(get_db)):
     user_by_id = db.query(models.User).filter(models.User.id == id).first()
     if not user_by_id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found")
     return user_by_id
 
-@app.delete('/user/{id}',status_code = status.HTTP_204_NO_CONTENT)
+@app.delete('/user/{id}',status_code = status.HTTP_204_NO_CONTENT,tags=["Users"])
 async def delete_user_by_id(id,db:Session = Depends(get_db)):
     user_by_id = db.query(models.User).filter(models.User.id == id).first()
     if not user_by_id:
@@ -95,4 +95,5 @@ async def delete_user_by_id(id,db:Session = Depends(get_db)):
     db.delete(user_by_id)
     db.commit()
     return {"details": f"User with id {id} deleted successfully"}
+
 
